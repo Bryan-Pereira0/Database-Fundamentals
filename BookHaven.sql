bb42ced2b0dd3aa2a3819472b4e3ed2853dfd4ae
bryanpereira <gfxmagicz@gmail.com>
2024-11-10 22:17:10 -0500

    first commit

---
+++ BookHaven.sql
@@ -0,0 +1,30 @@
+CREATE TABLE authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    price DECIMAL NOT NULL,
+    author_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE transactions(
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    book_id INT NOT NULL,
+    transaction_date DATE NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY(book_id) REFERENCES books(book_id)
+);
